ad585e87f1c2cae4310b0df798b4ec35a1e7caea
CrystalCry97 <amalsyahmi97@gmail.com>
2018-12-03 03:19:11 +0800

    Add and retriev data Course

--- public/dashboard/pages/edit.js
+++ public/dashboard/pages/edit.js
@@ -1,35 +1,32 @@
 
+    var rootref=firebase.database().ref().child("ClassSession");
 
-    var rootref=firebase.database().ref().child("class");
-    
-    rootref.on("child_added",snap=>{
-        console.log("jjj");
-        
-        
-     var code = snap.child("classcode").val(); 
-   
-     var name= snap.child("classname").val();
-    
-     $("#table_body").append("<tr><td>"+ code +"</td><td>"+ name + "</td><td><button>Delete</button></td></tr>");
-     $("#classoption").append("<option>"+ code +" " + name + "<option>");
-     
+    rootref.on("value", snap => {
+        snap.forEach(function(itemSnapshot) {
+          var code= itemSnapshot.key;
+          var courseName= rootref.child(code).child("classname");
+
+          courseName.once("value")
+          .then((snap2)=> {
+              var name= snap2.val();
+              console.log(code+ " " +name);
+              $("#table_body").append("<tr><td>"+ code +"</td><td>"+ name + "</td><td><button>Delete</button></td></tr>");
+              $("#classoption").append("<option>"+ code +" " + name + "<option>");
+          });
+
+        });
     });
-
-
+    
     function addclass(){
-        // $("#table_body").remove();
-        // $("#classoption").remove();
-        var addCourse=firebase.database().ref().child('class/class4');
+        location.reload();
 
         var add_code = document.getElementById("addcode").value;
         var add_name = document.getElementById("addname").value;
+
+        var addCourse=firebase.database().ref().child('ClassSession/'+add_code);
         
         /* Save and update data */
         addCourse.set({
-            classcode: add_code,
             classname: add_name
         });
-
-
-    
     }
