f22593325c9677b531a8fe99e9b6346df0c6397c
CrystalCry97 <amalsyahmi97@gmail.com>
2018-12-02 23:40:08 +0800

    change 8081

--- public/dashboard/pages/edit.js
+++ public/dashboard/pages/edit.js
@@ -1,26 +1,5 @@
 
 
-    // var rootRef = firebase.database().ref().child("class");
-
-    // rootRef.on("child_added",snap =>{
-    //     alert(snap.val());
-
-    //     var code = snap.child("classcode").val();
-    //     var name = snap.child("classname").val();
-
-    //     var table = document.getElementById("table_body");
-
-    //     var row = table.insertRow(0);
-    
-    //     var cell1 = row.insertCell(0);
-    
-    //     var cell2 = row.insertCell(1);
-      
-    //     cell1.innerHTML = code;
-    
-    //     cell2.innerHTML = name;
-    //     $("#table_body").append("<tr><td>"+ code +"</td><td>"+ name + "</td><td><button>Delete</button></td></tr>");
-    // });
     
     var rootref=firebase.database().ref().child("class");
     
