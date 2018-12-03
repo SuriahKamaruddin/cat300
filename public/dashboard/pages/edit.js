4fe94787598ad49dfd669430d69f5b204b0ac787
Suriah <suriahkamaruddin@gmail.com>
2018-12-03 11:46:22 +0800

    edit commit

--- public/dashboard/pages/edit.js
+++ public/dashboard/pages/edit.js
@@ -1,5 +1,5 @@
 
-    var rootref=firebase.database().ref().child("class");
+    var rootref=firebase.database().ref().child("ClassSession");
     
     rootref.on("child_added",snap=>{
         console.log("jjj");
@@ -19,14 +19,13 @@
     function addclass(){
         // $("#table_body").remove();
         // $("#classoption").remove();
-        var addCourse=firebase.database().ref().child('class/class4');
 
         var add_code = document.getElementById("addcode").value;
         var add_name = document.getElementById("addname").value;
-        
+
+        var addCourse=firebase.database().ref().child('ClassSession/'+add_code);
         /* Save and update data */
         addCourse.set({
-            classcode: add_code,
             classname: add_name
         });
     }
